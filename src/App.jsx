30e7239996c640f71e6c312071c239a8d5bda5eb
cevdril <150969909+cevdril@users.noreply.github.com>
2024-05-08 18:24:25 -0400

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Card from './components/Card'
-import Hero from ".components/Hero"
+import Card from './Components/Card'
+import Hero from "./Components/Hero"
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,14 +14,14 @@ function App() {
     {count}
     <button onClick={() => setCount(count +1)}>+</button>
     <Card 
-    image="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.voanews.com%2Fa%2Fblack-panther%2F4256531.html&psig=AOvVaw14vbIhTQuQU1gTjt7_Kvts&ust=1715291444361000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCJDQoN-E_4UDFQAAAAAdAAAAABAE"
+    image="https://i.pinimg.com/474x/0c/5a/2a/0c5a2afd4bdb78e2499d2b62f8ac9051.jpg"
     name="black panther"
     city="Jacmel"
     job="school"
     />
 
     <Card 
-    image="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwallpapers.com%2Fwallpapers%2Fcristiano-ronaldo-paint-effect-artwork-xssod3kr0av45zvs.html&psig=AOvVaw3oc5w8vH1bPtnE52AEuwUU&ust=1715291691331000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCMDEkNOF_4UDFQAAAAAdAAAAABAE"
+    image="https://publish-p47754-e237306.adobeaemcloud.com/adobe/dynamicmedia/deliver/dm-aid--46f84f92-3cda-444b-bb0e-605d50aa156f/_390575742906.app.png?preferwebp=true"
     name="cristiano ronaldo"
     city="santiago"
     job="soccer player"
